8d57b802f8ef0270cfd03172551f08290ae6c343
Alec McClure <mcclure@gamelabsstation.hostos.cuny.edu>
2017-09-12 10:28:55 -0400

    uploaded project folder as of week 2

---
+++ GD205_Fall2017/Assets/Scripts/Basic3D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic3D : MonoBehaviour {
+
+	public GameObject mover;
+	public Vector3 movementAmount;
+
+	// Use this for initialization
+	void Start () {
+		movementAmount = new Vector3 (1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Debug.Log ("true");
+			mover.transform.position += movementAmount;
+		} else {
+			Debug.Log ("false");
+		}
+	}
+}
