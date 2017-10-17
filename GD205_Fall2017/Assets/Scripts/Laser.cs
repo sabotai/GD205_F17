9b1f0c486368a9a507cd54b20f5f64f6507974f0
Alec McClure <sabotaione@gmail.com>
2017-10-17 12:13:13 -0400

    added week 7 (raycasting)

---
+++ GD205_Fall2017/Assets/Scripts/Laser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Ray beam = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		Debug.DrawRay (beam.origin, beam.direction * 1000f);
+
+		RaycastHit beamHit = new RaycastHit ();
+
+		if (Physics.Raycast (beam, out beamHit, 1000f)) {
+			Debug.Log ("you hit something at" + beamHit.point);
+			if (Input.GetMouseButtonDown (0)) {
+				beamHit.rigidbody.AddForce(Random.insideUnitSphere * 5000f);
+			}
+			if(Input.GetMouseButtonDown(1)){
+				Instantiate (prefab, beamHit.point, Quaternion.identity);
+			}
+			if (Input.GetMouseButtonDown (2)) {
+				Destroy(beamHit.collider.gameObject);
+			}
+
+		}
+	}
+}
