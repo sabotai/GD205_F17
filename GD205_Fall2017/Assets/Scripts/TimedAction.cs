d1e0423c78cf64b521e4d616f571efda8e64a622
Alec McClure <sabotaione@gmail.com>
2017-10-24 12:20:27 -0400

    added week 8

---
+++ GD205_Fall2017/Assets/Scripts/TimedAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedAction : MonoBehaviour {
+
+	public float startTime = 5f;
+	public float interval = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= startTime) {
+			startTime += interval;
+			Debug.Log ("doing something");
+
+			StartCoroutine (ScreenShake.Shake (0.25f, 0.5f));
+		}
+		
+	}
+}
