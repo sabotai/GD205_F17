42c0de96891d1109c13cf8e2010800faba76f869
Alec McClure <sabotaione@gmail.com>
2017-10-31 12:17:04 -0400

    added week 9 stuff

---
+++ GD205_Fall2017/Assets/Scripts/AnimatorInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorInput : MonoBehaviour {
+
+	Animator myAnim;
+	int current;
+
+	// Use this for initialization
+	void Start () {
+		myAnim = GetComponent<Animator> ();
+		current = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			current = 1;
+		} else {
+			current = 0;
+		}
+
+		myAnim.SetInteger ("whichState", current);
+	}
+}
