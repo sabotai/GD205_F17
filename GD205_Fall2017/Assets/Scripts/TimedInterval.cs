5dba189fb68d214326498839d0522fca1580972d
Alec McClure <sabotaione@gmail.com>
2017-10-31 09:15:34 -0400

    local changes

---
+++ GD205_Fall2017/Assets/Scripts/TimedInterval.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedInterval : MonoBehaviour {
+
+	//initial goTime means that's when it will start happening
+	public float goTime = 30;
+	//interval is how frequently it will run after that
+	public float interval = 3;
+
+	// Update is called once per frame
+	void Update () {
+		//checking if Time.time (time elapsed since the scene began in seconds) has surpassed our timer/alarm (goTime)
+		if (Time.time >= goTime){
+			//we add the interval amount to our goTime to reset the clock, otherwise it will run every frame
+			goTime += interval;
+
+			
+			//do the thing you want to happen at these intervals
+			//ScreenShake is the other script, and Shake() is a function of it
+			//the arguments are duration and magnitude of the shake
+			//StartCoroutine(ScreenShake.Shake(0.25f, 0.5f));
+			Debug.Log("do");
+		}
+
+	}
+}
